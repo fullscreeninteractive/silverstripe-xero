8b283e6d518afe34734031eb987dad30442ac6df
Will Rossiter <will@fullscreen.io>
2021-09-29 09:57:54 +1300

    Handle oauth exceptions gracefully.

--- src/XeroSiteConfigExtension.php
+++ src/XeroSiteConfigExtension.php
@@ -2,9 +2,11 @@
 
 namespace FullscreenInteractive\SilverStripeXero;
 
+use Exception;
 use SilverStripe\Core\Environment;
 use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\LiteralField;
 use SilverStripe\Forms\ReadonlyField;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Security\Permission;
@@ -39,19 +41,27 @@ class XeroSiteConfigExtension extends DataExtension
 
         if ($this->owner->XeroAccessToken) {
             $tenants = [];
-            $tenantRecords = XeroFactory::singleton()->getTenants($this->owner->XeroAccessToken);
 
-            foreach ($tenantRecords as $tenant) {
-                $tenants[$tenant->tenantId] = $tenant->tenantName;
+            try {
+                $tenantRecords = XeroFactory::singleton()->getTenants($this->owner->XeroAccessToken);
+
+                foreach ($tenantRecords as $tenant) {
+                    $tenants[$tenant->tenantId] = $tenant->tenantName;
+                }
+
+                $fields->addFieldsToTab('Root.Xero', [
+                    DropdownField::create(
+                        'XeroTenantId',
+                        'Xero Tenant',
+                        $tenants
+                    )
+                ]);
+            } catch (Exception $e) {
+                $fields->addFieldsToTab('Root.Xero', LiteralField::create(
+                    'XeroError',
+                    'An error has occured connecting to xero: '. $e->getMessage()
+                ));
             }
-
-            $fields->addFieldsToTab('Root.Xero', [
-                DropdownField::create(
-                    'XeroTenantId',
-                    'Xero Tenant',
-                    $tenants
-                )
-            ]);
         }
     }
 }
