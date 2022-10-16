7b538347d71d04f5146dd9dc03cd7139040192f5
Will Rossiter <will@fullscreen.io>
2022-10-17 08:38:04 +1300

    fix: queue up job directly

--- src/RefreshXeroTokenJob.php
+++ src/RefreshXeroTokenJob.php
@@ -2,6 +2,7 @@
 
 namespace FullscreenInteractive\SilverStripeXero;
 
+use SilverStripe\Core\Injector\Injector;
 use Symbiote\QueuedJobs\Services\AbstractQueuedJob;
 
 class RefreshXeroTokenJob extends AbstractQueuedJob
@@ -18,6 +19,12 @@ class RefreshXeroTokenJob extends AbstractQueuedJob
         $date = $xero->renewToken();
 
         $this->addMessage('Token refreshed until: '. date('d/m/Y H:i:s', $date));
+
+        $job = new RefreshXeroTokenJob();
+        Injector::inst()->get('Symbiote\QueuedJobs\Services\QueuedJobService')->queueJob(
+            $job,
+            date('Y-m-d H:i:s', "+10 minutes")
+        );
 
         $this->isComplete = true;
     }
