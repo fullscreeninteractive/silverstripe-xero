f6ef4b76a23a49b105835fb86a63794ef80d94a9
Will Rossiter <will@fullscreen.io>
2023-10-02 10:04:37 +1300

    fix: syntax in refresh job

--- src/RefreshXeroTokenJob.php
+++ src/RefreshXeroTokenJob.php
@@ -4,6 +4,7 @@ namespace FullscreenInteractive\SilverStripeXero;
 
 use SilverStripe\Core\Injector\Injector;
 use Symbiote\QueuedJobs\Services\AbstractQueuedJob;
+use Symbiote\QueuedJobs\Services\QueuedJobService;
 
 class RefreshXeroTokenJob extends AbstractQueuedJob
 {
@@ -18,12 +19,16 @@ class RefreshXeroTokenJob extends AbstractQueuedJob
         $xero = XeroFactory::create();
         $date = $xero->renewToken();
 
-        $this->addMessage('Token refreshed until: '. date('d/m/Y H:i:s', $date));
+        $this->addMessage('Token refreshed until: ' . date('d/m/Y H:i:s', $date));
 
         $job = new RefreshXeroTokenJob();
-        Injector::inst()->get('Symbiote\QueuedJobs\Services\QueuedJobService')->queueJob(
+
+        /** @var QueuedJobService */
+        $service = Injector::inst()->get(QueuedJobService::class);
+
+        $service->queueJob(
             $job,
-            date('Y-m-d H:i:s', "+10 minutes")
+            date('Y-m-d H:i:s', strtotime("+10 minutes"))
         );
 
         $this->isComplete = true;
